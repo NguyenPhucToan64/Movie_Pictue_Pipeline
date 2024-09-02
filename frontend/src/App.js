a19af8d115f07abe5a372a2ccc2c865d00107707
Toan <toanpclong.tpl@gmail.com>
2024-09-02 17:13:03 +0700

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,15 +8,36 @@ export default async function App() {
   console.log('APIf',apiUrl);
   
   const [selectedMovie, setSelectedMovie] = useState(null);
-  const [movies, setMovies] = useState(null);
+  const [movies, setMovies] = useState([
+    {
+      "id": "123",
+      "title": "Top Gun: Maverick"
+    },
+    {
+      "id": "456",
+      "title": "Sonic the Hedgehog"
+    },
+    {
+      "id": "789",
+      "title": "A Quiet Place"
+    }
+  ]);
 
   useEffect(async ()=>{
-    const response = await fetch(apiUrl);
-    const moviesfetch = await response.json();
-    console.log('Movies:', moviesfetch);
-    setMovies(moviesfetch)
+    async function fetchData() {
+      try {
+          const response = await fetch(apiUrl); // Sử dụng URL của API thực tế
+          const data = await response.json();
+          setMovies(data); // Cập nhật state abc với dữ liệu mới
+      } catch (error) {
+          console.error('Failed to fetch data: ', error);
+          // Xử lý lỗi tại đây nếu cần
+      }
+  }
 
-  },[movies])
+  fetchData();
+
+  },[])
 
   const handleMovieClick = (movie) => {
     setSelectedMovie(movie);
